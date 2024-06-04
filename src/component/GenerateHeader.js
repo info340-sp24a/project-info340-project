3db2b07813eac253cb5a9c154143b0858f32c591
Marc0Guo <jguo27@uw.edu>
2024-06-03 19:07:58 -0700

    update header bar

--- src/component/GenerateHeader.js
+++ src/component/GenerateHeader.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { getAuth, signOut } from 'firebase/auth';
+import '../index.css'; 
 
 export function HeaderBar({ currentUser, changeUserFunction }) {
+  const [menuOpen, setMenuOpen] = useState(false);
   const auth = getAuth();
   const navigate = useNavigate();
 
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
-        changeUserFunction(null); // Clear the user data
-        navigate('/signin'); // Redirect to the sign-in page
+        changeUserFunction(null); 
+        navigate('/signin'); 
       })
       .catch((error) => {
         console.error("Sign-out error:", error);
       });
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
 
   return (
@@ -23,7 +29,10 @@ export function HeaderBar({ currentUser, changeUserFunction }) {
         <h1><NavLink className="app-name" to="/">Ski Resorts</NavLink></h1>
         <p>Experience the thrill of the slopes like never before</p>
       </div>
-      <ul className="nav">
+      <div className="menu-icon" onClick={toggleMenu}>
+        &#9776;
+      </div>
+      <ul className={`nav ${menuOpen ? 'open' : ''}`}>
         <li className="nav-item">
           <NavLink className="nav-link" to="/">Home</NavLink>
         </li>
@@ -37,11 +46,9 @@ export function HeaderBar({ currentUser, changeUserFunction }) {
           <NavLink className="nav-link" to="/summary">Summary</NavLink>
         </li>
         {currentUser && currentUser.userId ? (
-          <>
-            <li className="nav-item">
-              <button className="btn btn-secondary ms-2" onClick={handleSignOut}>Sign Out</button>
-            </li>
-          </>
+          <li className="nav-item">
+            <button className="btn btn-secondary ms-2" onClick={handleSignOut}>Sign Out</button>
+          </li>
         ) : (
           <li className="nav-item">
             <NavLink className="nav-link" to="/signin">Sign In</NavLink>
